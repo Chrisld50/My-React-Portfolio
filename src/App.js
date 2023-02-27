3bf28574774ff8fcba13f03886ed2f71b2e12d25
Chrisld50 <chrisld50@yahoo.com>
2023-02-26 21:11:16 -0600

    made corrections for heroku

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
   
     <div style={{backgroundImage:`url(${Background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: 1050}} >
 
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <>
             <Routes>
-              <Route exact path='/My-React-Portfolio' element={<Home />} />
+              <Route exact path='/' element={<Home />} />
 
-              <Route exact path='/My-React-Portfolio/aboutme' element={<AboutMe />} />
+              <Route exact path='/aboutme' element={<AboutMe />} />
 
-              <Route path='/My-React-Portfolio/contact' element={<Contact />} />
+              <Route path='/contact' element={<Contact />} />
 
             </Routes>
           </>

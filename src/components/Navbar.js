315b8e65bd7fe3a9df9d2ee283d022691e212604
Chrisld50 <chrisld50@yahoo.com>
2023-02-26 20:03:45 -0600

    added gh pages and script

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ export default function Navigationbar() {
               <Nav.Link className={"text-light hover-zoom hover-overlay"} href="/">Home</Nav.Link>
               <Nav.Link className={"text-light"}  href="/aboutme">About Me</Nav.Link>
               <Nav.Link className={"text-light"} href="/contact">Contact</Nav.Link>
+              <Nav.Link className={"text-light"} href="https://docs.google.com/document/d/1i7YMFd_SLYgDZsmjsqvHGy8lhFztWt6ZG-dV-v69124/edit" target="_blank" >Resume</Nav.Link>
            </Nav>
           <Nav>
           <Nav.Link eventKey={2} href="">

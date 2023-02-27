cb87d41b8853b6bb02b7f85df03d77ae8b19e805
Chrisld50 <chrisld50@yahoo.com>
2023-02-26 21:01:13 -0600

    debugging

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Contact from './components/Contact';
 import AboutMe from './components/AboutMe';
 import Home from './components/Home';
